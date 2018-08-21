1568db8455ed8fa76248b51d61c7ec0b095247a3
dilanakbiyik <dilanyvz_93@hotmail.com>
2018-08-21 22:07:10 +0300

    Show clear button when input changed. Add clear button control. Change state with search changed.

--- src/components/Autocompleter/Autocompleter.js
+++ src/components/Autocompleter/Autocompleter.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import {
-    MdSearch
+    MdSearch,
+    MdClear
 } from 'react-icons/md/index';
 
 import './style.css';
 
 class Autocompleter extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            search: '',
+            showClearButton: false
+        };
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+    }
+    handleSearchChange(event) {
+        this.setState({
+            search: event.target.value,
+            showClearButton: !!event.target.value
+        });
+    }
     componentDidMount(){
         this.searchInput.focus();
     }
     render() {
+        const { showClearButton } = this.state;
+        let clearButton = null;
+        if(showClearButton === true){
+            clearButton = <MdClear size={25} />
+        }
         return (
             <div className="Autocompleter">
                 <div className="search-area">
                     <form>
-                        <input ref={(input) => { this.searchInput = input; }} type="text" placeholder={'Zoeken'} />
+                        <input ref={(input) => { this.searchInput = input; }}
+                               onChange={this.handleSearchChange}
+                               type="text"
+                               placeholder={'Zoeken'} />
                     </form>
+                    {clearButton}
                     <MdSearch size={25} />
                 </div>
             </div>
