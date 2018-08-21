5ba92b729bdc59fc1825e308d78510367de21ae7
dilanakbiyik <dilanyvz_93@hotmail.com>
2018-08-21 21:55:39 +0300

    Focus the search when it rendered.

--- src/components/Autocompleter/Autocompleter.js
+++ src/components/Autocompleter/Autocompleter.js
@@ -5,13 +5,16 @@ import {
 
 import './style.css';
 
-class Autocompleter extends React.Component { // eslint-disable-line react/prefer-stateless-function
+class Autocompleter extends React.Component {
+    componentDidMount(){
+        this.searchInput.focus();
+    }
     render() {
         return (
             <div className="Autocompleter">
                 <div className="search-area">
                     <form>
-                        <input type="text" placeholder={'Zoeken'} />
+                        <input ref={(input) => { this.searchInput = input; }} type="text" placeholder={'Zoeken'} />
                     </form>
                     <MdSearch size={25} />
                 </div>
